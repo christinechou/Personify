d6e3733f00e5338deecc70325f1099ff4e7f7a64
Christine Chou <christinejchou@gmail.com>
2016-09-27 19:24:41 -0700

    Add photos, additional tweaks to wording in first page and styling

--- client/app/text/text.js
+++ client/app/text/text.js
@@ -2,7 +2,7 @@ angular.module('narrative.text', ['narrative.services'])
 
 
 .controller('TextCtrl', function($scope, $location, Sentiment) { //injecting factory
-  $scope.sentiment = {};   
+  $scope.sentiment = {};
   // will look like: { title: 'xyz', text: 'string', text: 'text to analyze here'}
 
   $scope.generate = function(title, text) {
@@ -11,7 +11,7 @@ angular.module('narrative.text', ['narrative.services'])
       .then(function(tones) {
         $scope.sentiment.tones = tones;
         Sentiment.displayResults($scope.sentiment);
-        console.log('$scope.sentiment: ',$scope.sentiment)
+        //$scope.sentiment: [Object] {text: "TESTING!!!!!", title: "asdf", tones: Object}
         $location.path('/sentiment');
       })
   };
